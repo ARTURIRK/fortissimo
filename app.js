24d1198e788aad9394cabc263dfb7c1ec8f3a7d2
Artur Tsebaev <tsebaevartur38@gmail.com>
2022-09-02 12:24:42 +0300

    change

--- app.js
+++ app.js
@@ -1,6 +1,3 @@
-document.addEventListener("DOMContentLoaded", () => {
-  document.querySelector(".loader-cont").style.display = "flex";
-});
 window.addEventListener("load", () => {
   document.querySelector(".loader-cont").remove();
   document.querySelector(".all-content").style.display = "block";
