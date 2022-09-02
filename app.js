4f07d4d0176e4e863d0c59f89c7e14ce7f38014d
Artur Tsebaev <tsebaevartur38@gmail.com>
2022-09-02 12:20:13 +0300

    logs

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelector(".loader-cont").style.display = "flex";
+});
 window.addEventListener("load", () => {
   document.querySelector(".loader-cont").remove();
   document.querySelector(".all-content").style.display = "block";
